a0a90a1ba882168649bfdb6e01bc53d095ddba01
Nahla Galal <nahlaglal@gmail.com>
2020-05-06 23:41:59 +0200

    Update Assesment 3

--- js/script.js
+++ js/script.js
@@ -3,13 +3,6 @@ const dateSort = document.querySelector(".date-sort");
 const voteSort = document.querySelector(".vote-sort");
 const box = document.getElementById("listOfRequests");
 
-let request;
-if (window.XMLHttpRequest) {
-  request = new XMLHttpRequest();
-} else {
-  request = new ActiveXObject("Microsoft.XMLHTTP");
-}
-
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   let data = [];
@@ -27,57 +20,60 @@ form.addEventListener("submit", (e) => {
   };
 });
 
-const getVideoesList = () => {
-  request.open("GET", "http://localhost:7777/video-request");
-  request.onreadystatechange = () => {
-    if (request.status === 200 && request.readyState === 4) {
-      const res = JSON.parse(request.responseText);
+const getVideoesList = (sort_type = "date") => {
+  fetch(`http://localhost:7777/video-request?sort_type=${sort_type}`, {
+    method: "GET",
+    // params: {
+    //   sort_type,
+    // },
+    
+  })
+    .then((res) => res.json())
+    .then((res) => {
       box.innerHTML = "";
       res.forEach((element) => {
         box.innerHTML += `<div class="card mb-3" id=${element._id}>
-        <div class="card-body d-flex justify-content-between flex-row">
-          <div class="d-flex flex-column">
-            <h3>${element.topic_title}</h3>
-            <p class="text-muted mb-2">${element.topic_details}</p>
-            <p class="mb-0 text-muted">
-              ${
-                element.expected_result &&
-                `<strong>Expected results:</strong> ${element.expected_result}`
-              }
-            </p>
-          </div>
-          <div class="d-flex flex-column text-center">
-            <a class="btn btn-link" onclick="voteVideo('ups', '${
-              element._id
-            }')">🔺</a>
-            <h3 class="total-votes">${
-              element.votes.ups - element.votes.downs
-            }</h3>
-            <a class="btn btn-link" onclick="voteVideo('downs', '${
-              element._id
-            }')">🔻</a>
-          </div>
+      <div class="card-body d-flex justify-content-between flex-row">
+        <div class="d-flex flex-column">
+          <h3>${element.topic_title}</h3>
+          <p class="text-muted mb-2">${element.topic_details}</p>
+          <p class="mb-0 text-muted">
+            ${
+              element.expected_result &&
+              `<strong>Expected results:</strong> ${element.expected_result}`
+            }
+          </p>
         </div>
-        <div class="card-footer d-flex flex-row justify-content-between">
-          <div>
-            <span class="text-info">${element.status}</span>
-            &bullet; added by <strong>${element.author_name}</strong> on
-            <strong class="submit-date">${new Date(
-              element.submit_date
-            ).toDateString()}</strong>
-          </div>
-          <div class="d-flex justify-content-center flex-column 408ml-auto mr-2">
-            <div class="badge badge-success">${element.target_level}</div>
-          </div>
+        <div class="d-flex flex-column text-center">
+          <a class="btn btn-link" onclick="voteVideo('ups', '${
+            element._id
+          }')">🔺</a>
+          <h3 class="total-votes">${
+            element.votes.ups - element.votes.downs
+          }</h3>
+          <a class="btn btn-link" onclick="voteVideo('downs', '${
+            element._id
+          }')">🔻</a>
         </div>
-      </div>`;
+      </div>
+      <div class="card-footer d-flex flex-row justify-content-between">
+        <div>
+          <span class="text-info">${element.status}</span>
+          &bullet; added by <strong>${element.author_name}</strong> on
+          <strong class="submit-date">${new Date(
+            element.submit_date
+          ).toDateString()}</strong>
+        </div>
+        <div class="d-flex justify-content-center flex-column 408ml-auto mr-2">
+          <div class="badge badge-success">${element.target_level}</div>
+        </div>
+      </div>
+    </div>`;
       });
-    }
-  };
-  request.send();
+    });
 };
 
-window.addEventListener("load", getVideoesList);
+window.addEventListener("load", () => getVideoesList());
 
 const voteVideo = (vote_type, id) => {
   request.open("PUT", "http://localhost:7777/video-request/vote");
@@ -95,23 +91,11 @@ const voteVideo = (vote_type, id) => {
 dateSort.addEventListener("click", () => {
   dateSort.classList.add("active");
   voteSort.classList.remove("active");
-  const list = Array.from(box.querySelectorAll(".card")).sort((a, b) => {
-    let aDate = new Date(a.querySelector(".submit-date").innerText);
-    let bDate = new Date(b.querySelector(".submit-date").innerText);
-    return aDate < bDate ? 1 : aDate > bDate ? -1 : 0;
-  });
-  box.innerHTML = "";
-  list.forEach((listItem) => box.appendChild(listItem));
+  getVideoesList();
 });
 
 voteSort.addEventListener("click", () => {
   dateSort.classList.remove("active");
   voteSort.classList.add("active");
-  const list = Array.from(box.querySelectorAll(".card")).sort((a, b) => {
-    let aVotes = parseInt(a.querySelector(".total-votes").innerText);
-    let bVotes = parseInt(b.querySelector(".total-votes").innerText);
-    return aVotes < bVotes ? 1 : aVotes > bVotes ? -1 : 0;
-  });
-  box.innerHTML = "";
-  list.forEach((listItem) => box.appendChild(listItem));
+  getVideoesList("vote");
 });
--- server/index.js
+++ server/index.js
@@ -1,10 +1,10 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 const port = process.env.PORT || 7777;
-const VideoRequestData = require('./data/video-requests.data');
-const cors = require('cors');
-const mongoose = require('./models/mongo.config');
+const VideoRequestData = require("./data/video-requests.data");
+const cors = require("cors");
+const mongoose = require("./models/mongo.config");
 
 if (!Object.keys(mongoose).length) return;
 
@@ -12,35 +12,44 @@ app.use(cors());
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', (req, res) =>
-  res.send('Welcome to semicolon academy APIs, use /video-request to get data')
+app.get("/", (req, res) =>
+  res.send("Welcome to semicolon academy APIs, use /video-request to get data")
 );
 
-app.post('/video-request', async (req, res, next) => {
+app.post("/video-request", async (req, res, next) => {
   const response = await VideoRequestData.createRequest(req.body);
   res.send(response);
   next();
 });
 
-app.get('/video-request', async (req, res, next) => {
-  const data = await VideoRequestData.getAllVideoRequests();
+app.get("/video-request", async (req, res, next) => {
+  const { sort_type } = req.params;
+  console.log(req.params)
+  let data = await VideoRequestData.getAllVideoRequests();
+  if (sort_type === "vote") {
+    data = data.sort((a, b) => {
+      let aVotes = a.votes.ups - a.votes.downs;
+      let bVotes = b.votes.ups - b.votes.downs;
+      return aVotes < bVotes ? 1 : aVotes > bVotes ? -1 : 0;
+    });
+  }
   res.send(data);
   next();
 });
 
-app.put('/video-request', async (req, res, next) => {
+app.put("/video-request", async (req, res, next) => {
   const response = await VideoRequestData.updateRequest(req.body.id, req.body);
   res.send(response);
   next();
 });
 
-app.get('/users', async (req, res, next) => {
+app.get("/users", async (req, res, next) => {
   const response = await UserData.getAllUsers(req.body);
   res.send(response);
   next();
 });
 
-app.post('/users/login', async (req, res, next) => {
+app.post("/users/login", async (req, res, next) => {
   const response = await UserData.createUser(req.body);
   res.redirect(`http://localhost:5500?id=${response._id}`);
   next();
@@ -48,14 +57,14 @@ app.post('/users/login', async (req, res, next) => {
 
 app.use(express.json());
 
-app.put('/video-request/vote', async (req, res, next) => {
+app.put("/video-request/vote", async (req, res, next) => {
   const { id, vote_type } = req.body;
   const response = await VideoRequestData.updateVoteForRequest(id, vote_type);
   res.send(response);
   next();
 });
 
-app.delete('/video-request', async (req, res, next) => {
+app.delete("/video-request", async (req, res, next) => {
   const response = await VideoRequestData.deleteRequest(req.body.id);
   res.send(response);
   next();
